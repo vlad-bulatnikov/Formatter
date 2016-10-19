101a55570d3041905254c44436da25a8f9dd6fd4
Vladislav Bulatnikov <bulatnikov_94@mail.ru>
2016-10-20 00:38:15 +0600

    DZ#1: Formatter has been created

---
+++ src/com/company/FileWorker.java
@@ -0,0 +1,95 @@
+package com.company;
+
+import java.io.*;
+
+/**
+ * Created by vladislav on 19.10.16.
+ */
+public class FileWorker {
+
+    public static String read(String fileName) throws FileNotFoundException {
+
+        StringBuilder sb = new StringBuilder();
+        exists(fileName);
+        try {
+            File file = new File(fileName);;
+            BufferedReader in = new BufferedReader(new FileReader( file.getAbsoluteFile()));
+            try {
+                String s;
+                while ((s = in.readLine()) != null) {
+                    sb.append(s);
+                    sb.append("\n");
+                }
+            } finally {
+                in.close();
+            }
+        } catch(IOException e) {
+            throw new RuntimeException(e);
+        }
+        return sb.toString();
+    }
+
+    public static void formatter(String textFromFile) throws FileNotFoundException {
+
+        File file = new File("output");
+
+        try {
+            if(!file.exists()){
+                file.createNewFile();
+            }
+            PrintWriter out = new PrintWriter(file.getAbsoluteFile());
+            System.out.println(textFromFile);
+            String space = "";
+            boolean check = false;
+            for (int i = 0; i < textFromFile.length(); i++) {
+                String symbol = textFromFile.substring(i, i + 1);
+                if (symbol.equals(";")) {
+                    check = true;
+                    out.println(symbol);
+                    out.print(space);
+                }else {
+                if (symbol.equals("{")) {
+                    if(textFromFile.substring(i-1, i).equals(" ")) {
+                        space = space + "    ";
+                        out.println(symbol);
+                        out.print(space);
+                    } else {
+                        space = space + "    ";
+                        out.print(" ");
+                        out.println(symbol);
+                        out.print(space);
+                    }
+                } else {
+                    if (symbol.equals("}")) {
+                        if (check) {
+                            space = space.substring(0, space.length() - 4);
+                            //out.print(space);
+                            out.print(symbol);
+                            check=false;
+                        } else {
+                            space = space.substring(0, space.length() - 4);
+                            out.println("");
+                            out.print(space);
+                            out.print(symbol);
+                        }
+                    } else {
+                        out.print(symbol);
+                    }
+                }
+            }
+        }
+            out.close();
+        } catch(IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private static void exists(String fileName) throws FileNotFoundException {
+            File file = new File(fileName);
+            if (!file.exists()){
+                throw new FileNotFoundException(file.getName());
+            }
+    }
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 
+    private static String text = "This new text \nThis new text2\nThis new text3\nThis new text4\n";
+    private static String fileName = "./input";
+
+
     public static void main(String[] args) {
-	    System.out.println("Hello");
+        try {
+            String textFromFile = FileWorker.read(fileName);
+            System.out.println(textFromFile);
+            FileWorker.formatter(textFromFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
     }
 }
